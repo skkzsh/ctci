7948b749c9d4911266b90eb70c850b0ddc9cfb19
skkzsh <skkzsh12@gmail.com>
2022-10-05 22:17:18 +0900

    Solve 1.3

---
+++ q1_03/test_q1_03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pytest
+
+
+def urlify(s: str, num: int) -> str:
+    return s.rstrip().replace(' ', '%20')
+
+
+@pytest.mark.parametrize(
+    "s, num, expected", [
+        ("Mr John Smith   ", 13, "Mr%20John%20Smith"),
+    ]
+)
+def test(s: str, num: int, expected: str):
+    assert urlify(s, num) == expected
