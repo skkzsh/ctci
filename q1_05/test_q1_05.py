124fd22a946817f2cf34481a4234eaa492c1b116
skkzsh <skkzsh12@gmail.com>
2022-10-05 23:59:37 +0900

    Solve 1.5 only when insert or delete

--- q1_05/test_q1_05.py
+++ q1_05/test_q1_05.py
@@ -4,6 +4,24 @@ import pytest
 
 
 def is_one_away(sfrom: str, sto: str) -> bool:
+    if len(sfrom) - len(sto) == 1:
+        return is_one_away_plus(sfrom, sto)
+    elif len(sfrom) - len(sto) == -1:
+        return is_one_away_plus(sto, sfrom)
+    elif len(sfrom) - len(sto) == 0:
+        return sfrom == sto
+    return False
+
+def is_one_away_plus(longer: str, shorter: str) -> bool:
+    if len(longer) - len(shorter) != 1:
+        return False
+
+    c = (set(longer) - set(shorter)).pop()
+    for i in range(len(shorter) + 1):
+        candidate = shorter[:i] + c + shorter[i:]
+        if candidate == longer:
+            return True
+
     return False
 
 
