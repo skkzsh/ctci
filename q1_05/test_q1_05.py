e6c413f9d2a283b43b3d4810ad148590fa2d3f58
skkzsh <skkzsh12@gmail.com>
2022-10-06 00:19:50 +0900

    Solve 1.5 when replace, but bug exists

--- q1_05/test_q1_05.py
+++ q1_05/test_q1_05.py
@@ -9,8 +9,9 @@ def is_one_away(sfrom: str, sto: str) -> bool:
     elif len(sfrom) - len(sto) == -1:
         return is_one_away_plus(sto, sfrom)
     elif len(sfrom) - len(sto) == 0:
-        return sfrom == sto
+        return is_one_away_equal(sfrom, sto)
     return False
+
 
 def is_one_away_plus(longer: str, shorter: str) -> bool:
     if len(longer) - len(shorter) != 1:
@@ -25,11 +26,33 @@ def is_one_away_plus(longer: str, shorter: str) -> bool:
     return False
 
 
+def is_one_away_equal(s1: str, s2: str) -> bool:
+    if len(s1) - len(s2) != 0:
+        return False
+
+    only_from = set(s1) - set(s2)
+    only_to = set(s2) - set(s1)
+
+    if len(only_from) != 1 or len(only_to) != 1:
+        return False
+
+    c = only_from.pop()
+    for i in range(len(s2) + 1):
+        candidate = s2[:i] + c + s2[i + 1:]
+        if candidate == s1:
+            return True
+
+    return False
+
+
 @pytest.mark.parametrize(
     "sfrom, sto, expected", [
-        ("pale", "ple", True),
-        ("pale", "pales", True),
+        ("pale", "ale", True),
+        ("pale", "apale", True),
         ("pale", "bale", True),
+        ("pale", "pal", True),
+        ("pale", "palee", True),
+        ("pale", "pala", True),
         ("pale", "bake", False),
     ]
 )
