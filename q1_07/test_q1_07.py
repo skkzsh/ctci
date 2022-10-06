a799a3a9bd5ed267c0512b12728cf67c77266c02
skkzsh <skkzsh12@gmail.com>
2022-10-06 19:20:44 +0900

    Write test 1.7

---
+++ q1_07/test_q1_07.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from typing import List
+import pytest
+
+
+def rotate_matrix(matrix: List[List[int]]) -> List[List[int]]:
+    return matrix
+
+
+@pytest.mark.parametrize(
+    "matrix, expected", [(
+            [
+                [1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10],
+                [11, 12, 13, 14, 15],
+                [16, 17, 18, 19, 20],
+                [21, 22, 23, 24, 25]
+            ],
+            [
+                [21, 16, 11, 6, 1],
+                [22, 17, 12, 7, 2],
+                [23, 18, 13, 8, 3],
+                [24, 19, 14, 9, 4],
+                [25, 20, 15, 10, 5]
+            ]
+    )]
+)
+def test(matrix: List[List[int]], expected: List[List[int]]):
+    assert rotate_matrix(matrix) == expected
